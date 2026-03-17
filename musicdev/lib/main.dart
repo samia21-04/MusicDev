ccab3a0a91c420a8d84aee43e4823ce52de3060f
Samia <Samiathaynes@gmail.com>
2026-03-17 18:10:53 -0400

    Added screens, models,services, providers,widgets folders

--- musicdev/lib/main.dart
+++ musicdev/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 43, 3, 111)),
       ),
       home: const MyHomePage(title: 'Adaptive Focus Studio & Mood Soundscapes'),
     );
