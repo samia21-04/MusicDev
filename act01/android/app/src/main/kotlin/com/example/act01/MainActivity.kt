f297ecb82931d1456b7745b8d07c8b431460da86
Samia Haynes <samiathaynes@gmail.com>
2026-03-17 20:24:15 -0400

    Revert "Feature/music feature"

--- act01/android/app/src/main/kotlin/com/example/act01/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.act01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_database/android/app/src/main/kotlin/com/example/flutter_database/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_database
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
